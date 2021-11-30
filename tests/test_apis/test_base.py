fd8f4a167c7b071a4f80b03d963a71ce3d4fda8a
istoyanov <istoyanov@atlassian.com>
2021-11-30 19:29:26 +0200

    PIPES-859: Added unit tests to cover manual scale logic.

---
+++ tests/test_apis/test_base.py
@@ -0,0 +1,96 @@
+import subprocess
+from collections import namedtuple
+from unittest import TestCase, mock
+from json.decoder import JSONDecodeError
+
+import pytest
+import requests
+
+from exceptions import PipesHTTPError
+from tests.helpers import capture_output
+from apis.base import BearerAuth, BaseAPIService, BaseSubprocessAPIService
+
+
+class BearerAuthTestCase(TestCase):
+
+    def test_bearer_auth(self):
+        auth = BearerAuth('mytoken')
+        request = namedtuple('Request', ('headers', ))
+        request.headers = {}
+        auth(request)
+        self.assertEqual(request.headers['Authorization'], 'Bearer mytoken')
+
+
+class BaseAPIServiceTestCase(TestCase):
+
+    @mock.patch('requests.request')
+    def test_make_http_request(self, mock_request):
+        response_object = mock.MagicMock(status_code=201)
+        response_object.json.return_value = {'key': 'value'}
+
+        mock_request.return_value.__enter__.return_value = response_object
+        data, status = BaseAPIService().make_http_request('http://fake_url', method='post', json={'key': 'value'},
+                                                          headers={'Content-Application': 'json'})
+        self.assertEqual(status, 201)
+        self.assertEqual(data, {'key': 'value'})
+        mock_request.assert_called_once_with('post', 'http://fake_url', auth=mock.ANY, json={'key': 'value'},
+                                             headers={'Content-Application': 'json'},
+                                             timeout=5)
+
+    @mock.patch('requests.request')
+    def test_make_http_request_not_found(self, mock_request):
+        response_object = mock.MagicMock(status_code=404, text='')
+        response_object.raise_for_status.side_effect = requests.exceptions.HTTPError
+
+        mock_request.return_value.__enter__.return_value = response_object
+
+        with self.assertRaises(PipesHTTPError):
+            BaseAPIService().make_http_request('http://fake_url',)
+
+        response_object.json.side_effect = JSONDecodeError('Error decoding string', *('', 0))
+        data, status = BaseAPIService().make_http_request('http://fake_url', ignore_exc=(404, ))
+        self.assertEqual(status, 404)
+        self.assertEqual(data, '')
+
+
+class BaseSubprocessAPIServiceTestCase(TestCase):
+
+    @mock.patch('subprocess.run')
+    def test_run_command_fail(self, subprocess_mock):
+        subprocess_mock.side_effect = subprocess.CalledProcessError(cmd='test', returncode=1)
+        service = BaseSubprocessAPIService()
+        with capture_output() as out:
+            with pytest.raises(SystemExit) as pytest_wrapped_e:
+                service.run_command('test')
+
+        self.assertEqual(pytest_wrapped_e.type, SystemExit)
+        self.assertIn('Return code: 1', out.getvalue())
+
+    @mock.patch('apis.base.Popen')
+    def test_run_piped_command_fail(self, subprocess_mock):
+        process_mock = mock.Mock()
+        attrs = {'communicate.return_value': ('output', 'error')}
+        process_mock.configure_mock(**attrs)
+        subprocess_mock.return_value = process_mock
+        subprocess_mock.return_value.returncode = 1
+        service = BaseSubprocessAPIService()
+        with capture_output() as out:
+            with pytest.raises(SystemExit) as pytest_wrapped_e:
+                service.run_piped_command('test1 apply -foo', 'test2 apply -bar')
+
+        self.assertTrue(subprocess_mock.called)
+        self.assertEqual(pytest_wrapped_e.type, SystemExit)
+        self.assertIn('Subprocess: Return code for command: 1', out.getvalue())
+
+    @mock.patch('apis.base.Popen')
+    def test_run_piped_command_success(self, subprocess_mock):
+        process_mock = mock.Mock()
+        attrs = {'communicate.return_value': ('output', 'text')}
+        process_mock.configure_mock(**attrs)
+        subprocess_mock.return_value = process_mock
+        subprocess_mock.return_value.returncode = 0
+        service = BaseSubprocessAPIService()
+        result = service.run_piped_command('test1 apply -foo', 'test2 apply -bar')
+
+        self.assertTrue(subprocess_mock.called)
+        self.assertIn('output', result)
---
+++ tests/test_apis/test_kubernetes/test_base.py
@@ -0,0 +1,100 @@
+from unittest import TestCase, mock
+
+from constants import RUNNER_KUBERNETES_SPECS_DIR
+from apis.kubernetes.base import KubernetesBaseAPIService, KubernetesSpecFileAPIService
+
+
+class KubernetesBaseAPIServiceTestCase(TestCase):
+
+    @mock.patch('subprocess.run')
+    def test_delete_job(self, subprocess_mock):
+        subprocess_mock.return_value = mock.Mock(returncode=0)
+
+        api = KubernetesBaseAPIService()
+        api.delete_job('foo', 'bar')
+        subprocess_mock.assert_called_once_with(
+            ['kubectl', '-n', 'bar', 'delete', 'job', '-l', 'runnerUuid=foo'],
+            capture_output=True,
+            text=True,
+            timeout=10
+        )
+
+    @mock.patch('subprocess.run')
+    def test_get_kubernetes_version(self, subprocess_mock):
+        subprocess_mock.return_value = mock.Mock(returncode=0)
+
+        api = KubernetesBaseAPIService()
+        api.get_kubernetes_version()
+        subprocess_mock.assert_called_once_with(['kubectl', 'version'], capture_output=True, text=True, timeout=10)
+
+    @mock.patch('subprocess.run')
+    def test_get_kubernetes_config(self, subprocess_mock):
+        subprocess_mock.return_value = mock.Mock(returncode=0)
+
+        api = KubernetesBaseAPIService()
+        api.get_kubernetes_config()
+        subprocess_mock.assert_called_once_with(
+            ['kubectl', 'config', 'view'],
+            capture_output=True,
+            text=True,
+            timeout=10
+        )
+
+    @mock.patch('subprocess.run')
+    def test_get_or_create_kubernetes_namespace(self, subprocess_mock):
+        process_mock = mock.Mock('foo', returncode=0, stdout='bar')
+
+        subprocess_mock.return_value = process_mock
+        subprocess_mock.return_value.check_returncode = mock.Mock('foo', returncode=0, stdout='bar')
+
+        api = KubernetesBaseAPIService()
+        result = api.get_or_create_kubernetes_namespace()
+        self.assertEqual('bar', result)
+
+    @mock.patch.object(KubernetesBaseAPIService.api, 'run_piped_command')
+    def test_create_kubernetes_namespace(self, mock_create):
+        mock_create.return_value = 'baz'
+
+        api = KubernetesBaseAPIService()
+        result = api.create_kubernetes_namespace()
+        self.assertEqual('baz', result)
+
+
+class KubernetesSpecFileAPIServiceTestCase(TestCase):
+
+    @mock.patch('subprocess.run')
+    def test_apply_kubernetes_spec_file(self, subprocess_mock):
+        subprocess_mock.return_value = mock.Mock(returncode=0)
+
+        api = KubernetesSpecFileAPIService()
+        api.apply_kubernetes_spec_file('foo', 'bar')
+        subprocess_mock.assert_called_once_with(
+            ['kubectl', '-n', 'bar', 'apply', '-f', 'foo'],
+            capture_output=True,
+            text=True,
+            timeout=10
+        )
+
+    @mock.patch('apis.kubernetes.base.open')
+    def test_create_kube_spec_file(self, mocked_create):
+        runner_data = {
+            'accountUuid': 'account',
+            'repositoryUuid': 'repo',
+            'runnerUuid': 'runner',
+            'oauthClientId_base64': 'testID',
+            'oauthClientSecret_base64': 'testSecret',
+            'runnerNamespace': 'namespace'
+        }
+        api = KubernetesSpecFileAPIService()
+
+        api.create_kube_spec_file(runner_data)
+
+        mocked_create.assert_called_once_with(f'{RUNNER_KUBERNETES_SPECS_DIR}/runner-runner.yaml', 'w')
+
+    @mock.patch('pathlib.Path.unlink')
+    def test_delete_kube_spec_file(self, mocked_delete):
+        api = KubernetesSpecFileAPIService()
+
+        api.delete_kube_spec_file('test')
+
+        mocked_delete.assert_called_once_with(missing_ok=True)
